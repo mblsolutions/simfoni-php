efc1983dc7636f6fd702b6d79e61e0f472103e79
Christian Innerbichler <christian.innerbichler@gmail.com>
2022-06-24 15:00:12 +0100

    add verify signature request test

--- tests/VerifyRequestTest.php
+++ tests/VerifyRequestTest.php
@@ -28,6 +28,17 @@ class VerifyRequestTest extends TestCase
         self::assertTrue($verifyRequest->validates($payload));
     }
 
+    /** @test **/
+    public function can_verify_request_signature(): void
+    {
+        $json = "{\"event\":\"order.complete\",\"created\":1655986287,\"live\":false,\"version\":\"v4.12.15\",\"data\":{\"order_id\":55961284,\"hash\":\"eyJpdiI6IjlLb0VLSCtINVcxYlBCeFFIV1NhZGc9PSIsInZhbHVlIjoiVGtCSE1PbllydjJLUzE5MXlrVUJ2dz09IiwibWFjIjoiZGJkYTc4N2NhZTJmNzk2NWYwNzZkY2NkYTg2MWFlNGYyYzRkMzU3ZmJiNTA3MjI5NTQ3NzQ2YTU2ZDA1MDFjYiJ9\"}}";
+        $request = json_decode($json, true, 512, JSON_THROW_ON_ERROR);
+
+        $validator = new VerifyRequest('Yih7Ry8MkNaFPfzv6S4ZMCMC59FWMfQl', '1655986287,fa0b95dd83f2a783967d1cabb4a58f09716c6c87ed9f65a3c1e3f4d3061430cc');
+
+        $this->assertTrue($validator->validates($request));
+    }
+
     /** @test */
     public function invalid_requests_return_as_invalid(): void
     {
